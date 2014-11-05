535999b9e77903ea0cb745151520b1f666213730
hugoabonizio <hugo_abonizio@hotmail.com>
2014-11-05 23:20:52 +0000

    separate the fields in tabs

--- templates/matriz.php
+++ templates/matriz.php
@@ -5,18 +5,35 @@
 
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
   <h1 class="page-header">Matriz SWOT</h1>
+  
+  <ul class="nav nav-tabs" role="tablist">
+    <li class="active"><a href="#fortes" role="tab" data-toggle="tab">Pontos Fortes</a></li>
+    <li><a href="#fracos" role="tab" data-toggle="tab">Pontos Fracos</a></li>
+    <li><a href="#oportunidades" role="tab" data-toggle="tab">Oportunidades</a></li>
+    <li><a href="#ameacas" role="tab" data-toggle="tab">Ameaças</a></li>
+  </ul>
+  
+  <div class="tab-content" class="tab-content">
+    <div class="tab-pane active" id="fortes">
+      <h3 class="page-header">Pontos Fortes</h3>
+      <textarea style="width: 100%; height: 40%;" id="swot_pforte"><?= $rows['swot_pforte']; ?></textarea>
+    </div>
 
-  <h3 class="page-header">Pontos Fortes</h3>
-  <textarea style="width: 100%; height: 100px" id="swot_pforte"><?= $rows['swot_pforte']; ?></textarea>
+    <div class="tab-pane" id="fracos">
+      <h3 class="page-header">Pontos Fracos</h3>
+      <textarea style="width: 100%; height: 40%;" id="swot_pfraco"><?= $rows['swot_pfraco']; ?></textarea>
+    </div>
 
-  <h3 class="page-header">Pontos Fracos</h3>
-  <textarea style="width: 100%; height: 100px" id="swot_pfraco"><?= $rows['swot_pfraco']; ?></textarea>
+    <div class="tab-pane" id="oportunidades">
+      <h3 class="page-header">Oportunidades</h3>
+      <textarea style="width: 100%; height: 40%;" id="swot_oportunidades"><?= $rows['swot_oportunidades']; ?></textarea>
+    </div>
 
-  <h3 class="page-header">Oportunidades</h3>
-  <textarea style="width: 100%; height: 100px" id="swot_oportunidades"><?= $rows['swot_oportunidades']; ?></textarea>
-
-  <h3 class="page-header">Ameaças</h3>
-  <textarea style="width: 100%; height: 100px" id="swot_ameacas"><?= $rows['swot_ameacas']; ?></textarea>
+    <div class="tab-pane" id="ameacas">
+      <h3 class="page-header">Ameaças</h3>
+      <textarea style="width: 100%; height: 40%;" id="swot_ameacas"><?= $rows['swot_ameacas']; ?></textarea>
+    </div>
+  </div>
 </div>
 
 
