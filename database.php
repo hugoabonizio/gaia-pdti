4f84862f48529189a3d3d70f0ee504580454abe2
hugoabonizio <hugo_abonizio@hotmail.com>
2015-03-24 17:23:37 +0000

    change database

--- database.php
+++ database.php
@@ -8,12 +8,14 @@ class DB {
 		if (true) {
 			//$conn = "pgsql:host=ec2-174-129-197-200.compute-1.amazonaws.com port=5432 user=ldphxungmtlhvu password=te9bZEW2Xhemc8EDFsMQ3VbhZt dbname=dfi2fr0b3mm56t";
       $host = gethostbyname('ec2-174-129-197-200.compute-1.amazonaws.com');
-      $conn = "pgsql:host=$host port=5432 dbname=dfi2fr0b3mm56t";
+      //$conn = "pgsql:host=$host port=5432 dbname=dfi2fr0b3mm56t";
+      $conn = "pgsql:host=127.0.0.1 port=5432 dbname=hugo";
 		} else {
 			$conn = "sqlite:db/database.sqlite3";
 		}
     $s = microtime(true);
-		$this->pdo = new PDO($conn, 'ldphxungmtlhvu', 'te9bZEW2Xhemc8EDFsMQ3VbhZt', array(PDO::ATTR_TIMEOUT => 10, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		//$this->pdo = new PDO($conn, 'ldphxungmtlhvu', 'te9bZEW2Xhemc8EDFsMQ3VbhZt');
+    $this->pdo = new PDO($conn, 'hugo', 'senha123');
 		$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     $e = microtime(true);
