42cd0290d7047ed6d339f6716ee08887818fbb1a
hugoabonizio <hugo_abonizio@hotmail.com>
2015-03-24 18:39:52 +0000

    fix the link_to helper

--- helpers.php
+++ helpers.php
@@ -9,7 +9,7 @@ function link_to($url) {
 	if (is_production()) {
 		return "/index.php/" . $url;
 	} else {
-		return "http://localhost/slim_tests/2/index.php/" . $url;
+		return "http://gaia3.uel.br/projetos/gaia-pdti/index.php/" . $url;
 	}
 }
 
